d25449861298aa41295b3cfdb1d24cb69ceeb836
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-11 21:34:32 -0800

    Added hours page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ hours/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>Hours - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>Hours</h1>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>Hours</h1>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  Click <a href="/hours/2013-2014%20Hours.xlsx">here</a> to download the hours spreadsheet.
+	</div>
+      </div>
+
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
