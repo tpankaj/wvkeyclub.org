fee6ea78ade91f3d83684b7bae8583620f19c790
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-11 20:07:26 -0800

    Added rest of exec board
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- officers/index.php
+++ officers/index.php
@@ -57,7 +57,7 @@
 	</div>
 	<div class="eight columns">
 	  <p>
-	    I’m Michael Zhang, WVKC’s 2013-2014 President. I’ve been a part of Key Club since freshman year, and have enjoyed every minute. Previously the club’s webmaster, I’ve been focused on inspiring greater involvement from YOU, the people that make this club great. Whether it's a community run, Fall Rally or a festival, I always look forward to seeing our members dedicate themselves to service and even take on roles of leadership. I’m super excited for the year ahead; it’s full of opportunities to grow Key Club, from our divisional involvement, to our member count or our fundraising total. I’m always open to feedback, so feel free to send me an email or Facebook message if you have any suggestions or problems! I’m looking forward to the year ahead!
+	    I'm Michael Zhang, WVKC's 2013-2014 President. I've been a part of Key Club since freshman year, and have enjoyed every minute. Previously the club's webmaster, I've been focused on inspiring greater involvement from YOU, the people that make this club great. Whether it's a community run, Fall Rally or a festival, I always look forward to seeing our members dedicate themselves to service and even take on roles of leadership. I'm super excited for the year ahead; it's full of opportunities to grow Key Club, from our divisional involvement, to our member count or our fundraising total. I'm always open to feedback, so feel free to send me an email or Facebook message if you have any suggestions or problems! I'm looking forward to the year ahead!
 	  </p>
 	</div>
       </div>
@@ -79,7 +79,7 @@
 	</div>
 	<div class="eight columns">
 	  <p>
-	    Hi! I'm Iris, and I have officially crept from my position as Treasurer to the position of your Vice President for the 2013 - 2014 KC year. How glorious! I've been a dedicated member/officer since freshman year, accumulating 300 hours since. From simple elementary school festivals to California-Nevada-Hawaii's District-wide Convention, my passion to serve not only the community, but you guys as members has only grown&hellip;exponentially. In the past two years, my experience serving on both the Appointed and Executive Board has opened my eyes to the magic that happens behind-the-scenes&mdash;whether it’s staying up until 2am to find just one more volunteer opportunity, or communicating with our division-district liaisons to plan the perfect bake-sale, the hard work that's put into Key Club from members &amp; officers alike truly redefines the word "service". Outside of volunteering, I love Photobooth, bananas, and rhythmic gymnastics. I am also a huge advocate of Starbucks’ White Chocolate Mocha Frappuccinos, so do me a favor and try one please. Aside from everything, I cannot wait to see our club grow even more this year, and I hope, sometime in the next 12 months, that you will find your passion to serve. :) This is just the beginning.
+	    Hi! I'm Iris, and I have officially crept from my position as Treasurer to the position of your Vice President for the 2013 - 2014 KC year. How glorious! I've been a dedicated member/officer since freshman year, accumulating 300 hours since. From simple elementary school festivals to California-Nevada-Hawaii's District-wide Convention, my passion to serve not only the community, but you guys as members has only grown&hellip;exponentially. In the past two years, my experience serving on both the Appointed and Executive Board has opened my eyes to the magic that happens behind-the-scenes&mdash;whether it's staying up until 2am to find just one more volunteer opportunity, or communicating with our division-district liaisons to plan the perfect bake-sale, the hard work that's put into Key Club from members &amp; officers alike truly redefines the word "service". Outside of volunteering, I love Photobooth, bananas, and rhythmic gymnastics. I am also a huge advocate of Starbucks' White Chocolate Mocha Frappuccinos, so do me a favor and try one please. Aside from everything, I cannot wait to see our club grow even more this year, and I hope, sometime in the next 12 months, that you will find your passion to serve. :) This is just the beginning.
 	  </p>
 	</div>
       </div>
@@ -101,7 +101,57 @@
 	</div>
 	<div class="eight columns">
 	  <p>
-	    I’m Lily Vo and I’m Westview Key Club’s 2013-2014 Secretary. I’ve been a dedicated Key Clubber since Freshman year; holding positions such as Freshman and Sophomore Director. As my first year as an executive board member, I am honored to be sharing another unforgettable Key Club year with my fellow friends and peers. With two years of experience on the board, I am ready to move one step further to fulfill my role as Secretary and the responsibilities and duties that entails. My Key Club passion sparked at the CNH District Convention in Anaheim 2013. What was once a passive, obligatory attitude to wake up at the crack of dawn and pass out t shirts and water cups became an obsession, a new profound love to all things Key Club. I began going to divisional events, meeting and collaborating with people who share the same love and dedication to Key Club, and offered my time and effort to the community on a much larger scale. My goal is to help you discover your Key Club "spark". The spark that will allow you to experience all the same, hopefully even more, experiences I am fortunate enough to hold close to my heart. Make the most out of your next Key Club year and welcome to our crazy, but energetic, ohana!
+	    I'm Lily Vo and I'm Westview Key Club's 2013-2014 Secretary. I've been a dedicated Key Clubber since Freshman year; holding positions such as Freshman and Sophomore Director. As my first year as an executive board member, I am honored to be sharing another unforgettable Key Club year with my fellow friends and peers. With two years of experience on the board, I am ready to move one step further to fulfill my role as Secretary and the responsibilities and duties that entails. My Key Club passion sparked at the CNH District Convention in Anaheim 2013. What was once a passive, obligatory attitude to wake up at the crack of dawn and pass out t shirts and water cups became an obsession, a new profound love to all things Key Club. I began going to divisional events, meeting and collaborating with people who share the same love and dedication to Key Club, and offered my time and effort to the community on a much larger scale. My goal is to help you discover your Key Club "spark". The spark that will allow you to experience all the same, hopefully even more, experiences I am fortunate enough to hold close to my heart. Make the most out of your next Key Club year and welcome to our crazy, but energetic, ohana!
+	  </p>
+	</div>
+      </div>
+
+      <!-- Shaoxiong Liu, Treasurer-->
+      <div class="row">
+	<div class="four columns">
+	  <div class="row">
+	    <div class="twelve columns">
+	      <img src="/officers/images/liu-shaoxiong.jpg" />
+	    </div>
+	  </div>
+	  <div class="row">
+	    <div class="twelve columns" style="text-align:center;">
+	      <h4><a href="mailto:shaoxiongliu2012@gmail.com">Shaoxiong Liu</a><br />
+	      Treasurer</h4>
+	    </div>
+	  </div>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hello, I'm Shaoxiong Liu and I am your 2013-2014 Key Club Treasurer. Since the due date for the biography was some day in April, I obviously couldn't finish. I also blame Alex Zhang for starting the Bio System. I would like to start off by challenging each and everyone one of YOU to find a way to serve and to give back to the community to create change. Do not be afraid to attend events just because you are scared because you do not know anyone who is going. We are all one big family and even a complete stranger can turn into a great friend in just a few moments time. I do also encourage everyone to attend divisional events as you get to meet many new people from other schools.
+	  </p>
+	  <p>
+	    Outside of Key Club, I enjoy browsing the internet, swimming, and pursuing my hobby of photography. In the upcoming year, be on the lookout for a lot more fundraisers and fun events to get you more involved. If you ever have any suggestions or ideas for fundraisers please feel free to contact me as I am completely open to new and exciting ideas. I look forward to a great year! Last but not least, KEY CLUBBERS, HOW DO YOU FEEL?
+	  </p>
+	  <p>
+	    "If you can count your money, you don't have a billion dollars."
+	  </p>
+	</div>
+      </div>
+
+      <!-- Alec Chen -->
+      <div class="row">
+	<div class="four columns">
+	  <div class="row">
+	    <div class="twelve columns">
+	      <img src="/officers/images/chen-alec.jpg" />
+	    </div>
+	  </div>
+	  <div class="row">
+	    <div class="twelve columns" style="text-align:center;">
+	      <h4><a href="mailto:alec150@gmail.com">Alec Chen</a><br />
+	      Bulletin Editor</h4>
+	    </div>
+	  </div>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hey guys! My name is Alec Chen and I'll be your 2013-2014 Key Club Bulletin Editor. My job is to keep members updated with the most recent news by sending a newsletter out every month. I hope to improve Key Club and make it one of the best clubs on campus by working with my fellow officers to improve activity inside and outside of the club. My goal for this year is for all of you to become more devoted and active members, and also helping people understand the value of giving back to the community. If you guys need to contact me, contact me an email or send me an email. I'll be more than glad to take suggestions on the newsletter!
 	  </p>
 	</div>
       </div>
