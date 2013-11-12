5c83941ef0e48123a419709208de9192e256c7d7
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-12 12:36:22 -0800

    Added registration forms
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ forms/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>Forms - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>Forms</h1>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>Forms</h1>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <p>
+	    To become a registered member, turn in all of these forms along with the $13 dues by November 21, 2013. After this date, the dues will go up to $15.
+	  </p>
+	  <p>
+	    <a href="/forms/Trip%20Permit.pdf">Trip Permit</a><br />
+	    <a href="/forms/Registration%20Form.pdf">Registration Form</a>
+	  </p>
+	</div>
+      </div>
+
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
