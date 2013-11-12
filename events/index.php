2a8ef00eb47c20fd2c94e216a42ebcffb505e306
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-11 21:24:25 -0800

    Added events page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ events/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>Events - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>Events</h1>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>Events</h1>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <iframe src="https://www.google.com/calendar/embed?src=d51haimqra31ucq9tkabrng9us%40group.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	</div>
+      </div>
+
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
