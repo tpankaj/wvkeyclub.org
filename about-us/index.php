0025ae77038389bb6351d945bdb8390fa5345722
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-18 10:42:57 -0800

    Created initial about page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ about-us/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>About Us - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>About Us</h1>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>About Us</h1>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="nine columns">
+	  <h3>What Is Key Club?</h3>
+	</div>
+	<div class="three columns">
+	  <h4> More Information</h4>
+	</div>
+      </div>
+
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
