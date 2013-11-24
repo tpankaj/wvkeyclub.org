052af682bf2e3694c32af4fc267dd2323378db72
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-23 22:57:42 -0800

    Added LoL for PTP
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ lol-for-ptp/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>LoL For PTP - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>LoL For PTP Tournament</h1>
+	  <h2>Entry Payment</h2>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>LoL For PTP Tournament</h1>
+	  <h2>Entry Payment</h2>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <p>
+	    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+	      <input type="hidden" name="cmd" value="_s-xclick">
+	      <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHiAYJKoZIhvcNAQcEoIIHeTCCB3UCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCVQqeJO/A+Rer9t+H5ybtSAuvJQIONLlF3NGnKKQDv1+GKSN0c2LBSZVvbuv9/MM/93WYNNRH5Xu5w0RTR37as7rAji8NlavDbpkH88JITP4E0KEkzyyej5jyfehIBI/2/SE5q/DQ/t62GQ7q8u+Jx0WWotufwsYI+T/4RCxr1TzELMAkGBSsOAwIaBQAwggEEBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECF1gIPGujN47gIHghgM03u3Mp1g5H1dAufELUUNtqsLfJDairYYoG9W/3CRw2PWnPOD/gisabGNRwQKAmD01Xq9BUEQerOtGXLwtvJQwpMk1q6sVdvNsszfh0NV8Y1b4DDrhZazxWXuOH11h1KIN+Z3VwERKu/xcEOCGW+1ZPnnlJ2DVHEfi3EAgEZKpv9j5UxrJDnC65WR4YVn1RtctRhPd9u7mx7NWGoZcFAlWPPsrHEN0H1HTEgzQ84H66uKlqa/YvsV8121UwTFuhlwtudNhS+mEInyz/2c/e7UbyE3YXMUp6/E1mKsRjjGgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMzExMjQwNTQ3MDlaMCMGCSqGSIb3DQEJBDEWBBSDflKQMianjz1Qdi0G0TdVzXVlUzANBgkqhkiG9w0BAQEFAASBgClyCdGGgXaMR278j12WG1Auh1lcSzxzlK4ED2QwF96VmNV8OsNu/foTIt3hrCuylqflALnZvryM4/Np2BpLm12F94qIopfjR51wN65mm4tiI7A7Yt/K+IiKgvGt8+rzRBr59YwwlRM4vpdGZAvC/Z8vMm7MBrlFphLid/UU6DqZ-----END PKCS7-----">
+	      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	    </form>
+	  </p>
+	</div>
+      </div>
+
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
