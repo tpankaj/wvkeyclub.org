fc75a443cdc70aeef3e6ce7b9f4f4faae4f63488
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-24 13:45:54 -0800

    Removed sensitive info printout
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/test.php
+++ hours/test.php
@@ -3,6 +3,6 @@
    $db = $m->wvkeyclub;
    $members_collection = $db->members;
    $events_collection = $db->events;
-   var_dump($members_collection->findOne());
-   var_dump($events_collection->findOne());
+   //var_dump($members_collection->findOne());
+   //var_dump($events_collection->findOne());
    ?>
