6018d27497c80f960bd6f5b917a777b32141c5f9
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-27 22:48:53 -0800

    Removed another unnecessary var type in hours admin
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/admin/addeventhours_process.php
+++ hours/admin/addeventhours_process.php
@@ -2,7 +2,7 @@
    $m = new MongoClient("mongodb://localhost");
    $db = $m->wvkeyclub;
 
-   for ($int i = 1; $i <= $_POST["attendee-count"]; $i++)
+   for ($i = 1; $i <= $_POST["attendee-count"]; $i++)
    {
 	db->members->update({ fname : $_POST["member-fname-" . $i], lname : $_POST["member-lname-" . $i] }, { $push : { hours : { event_id : $_POST["event-id"], hours : $_POST["member-hours-" . $i] } } });
    }
