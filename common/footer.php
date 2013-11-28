6fbb9a3b69342beac2eccc1e4b3ccadf6150d647
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-28 13:21:37 -0800

    Added member list to hours admin and removed foundation.migrate
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- common/footer.php
+++ common/footer.php
@@ -1,6 +1,5 @@
 <script src="/js/vendor/jquery.js"></script>
 <script src="/js/foundation.min.js"></script>
-<script src="/js/foundation.migrate.js"></script>
 <script>
   $(document).foundation();
 </script>
---
+++ hours/admin/memberlist.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>Member List -  Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>Hours Admin Panel</h1>
+	  <h2>Member List</h2>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row">
+	<div class="large-12 columns">
+	  <table id="memberlist" class="tablesorter">
+	    <thead>
+	      <tr>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Email Address</th>
+		<th>Graduation Year</th>
+		<th>Position</th>
+		<th>Registered?</th>
+	    </thead>
+	    <tbody>
+	      <?php
+		 $m = new MongoClient("mongodb://localhost");
+		 $db = $m->wvkeyclub;
+
+	         $results = $db->members->find();
+
+                 foreach ($results as $member)
+                 {
+		 ?>
+	      <tr>
+		<td><?php echo $member["fname"]; ?></td>
+		<td><?php echo $member["lname"]; ?></td>
+		<td><a href="mailto:<?php echo $member["email"]; ?>"><?php echo $member["email"]; ?></a></td>
+		<td><?php echo $member["graduation_year"]; ?></td>
+		<td>
+		  <?php
+		     if ($member["position"] == "member")
+		        echo "Member";
+		     else if ($member["position"] == "executive-board")
+		        echo "Executive Board";
+		     else if ($member["position"] == "appointed-board")
+		        echo "Appointed Board";
+		     else
+		        echo $member["position"];
+		     ?>
+		</td>
+		<td>
+		  <?php
+		     if ($member["registered"])
+		        echo "Yes";
+		     else
+		        echo "No";
+		     ?>
+		</td>
+	      </tr>
+	    </tbody>
+	  </table>
+	</div>
+      </div>
+      
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+    <script src="/js/jquery.tablesorter.min.js"></script>
+    <script>
+      $(document).ready(function()
+         {
+            $("#memberlist").tablesorter();
+         }
+      );
+    </script>
+  </body>
+
+</html>
