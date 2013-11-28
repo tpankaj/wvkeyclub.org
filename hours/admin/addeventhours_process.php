fedd971c194759a8f4076c347cf70d8fb98f47b1
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-11-27 22:53:53 -0800

    Converted mongo to php in hours admin
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/admin/addeventhours_process.php
+++ hours/admin/addeventhours_process.php
@@ -4,7 +4,7 @@
 
    for ($i = 1; $i <= $_POST["attendee-count"]; $i++)
    {
-	$db->members->update({ "fname" : $_POST["member-fname-" . $i], "lname" : $_POST["member-lname-" . $i] }, { '$push' : { "hours" : { "event_id" : $_POST["event-id"], "hours" : $_POST["member-hours-" . $i] } } });
+	$db->members->update(array("fname" => $_POST["member-fname-" . $i], "lname" => $_POST["member-lname-" . $i]), array('$push' => array("hours" => array("event_id" => $_POST["event-id"], "hours" => $_POST["member-hours-" . $i]))));
    }
 
    header("Location: http://" . $_SERVER["SERVER_NAME"] . "/hours/admin/index.php?message=addeventhours_success");
