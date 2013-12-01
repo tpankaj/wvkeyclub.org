2e6901908d83e448ffc5c5ddf230c24079ddaddc
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-12-01 15:46:18 -0800

    Fixed typo in deletion modal
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- events/admin/deleteevent_confirm.php
+++ events/admin/deleteevent_confirm.php
@@ -1,4 +1,4 @@
 <h2>Delete Event</h2>
 <p>Are you sure you want to delete <?php echo $_GET["name"]; ?>?</p>
-<p><a href="/events/admin/deleteevent_process.php?id=<?php echo $_GET["id"]; ?>" class="button radius">Yes</a> <a href="$('#delete-modal').foundation('reveal', 'close')" class="button radius">No</a></p>
+<p><a href="/events/admin/deleteevent_process.php?id=<?php echo $_GET["id"]; ?>" class="button radius">Yes</a> <a href="javascript:$('#delete-modal').foundation('reveal', 'close')" class="button radius">No</a></p>
 <a class="close-reveal-modal">&#215;</a>
