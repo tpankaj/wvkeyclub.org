edfa1ad676946c755a6c2c3f8267f0826e4b9efb
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-12-05 22:14:40 -0800

    Updated event deletion to purge hours as well
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- events/admin/deleteevent_process.php
+++ events/admin/deleteevent_process.php
@@ -4,6 +4,12 @@
 
    $event_id = new MongoId($_GET["id"]);
 
+   $members = $db->members->find();
+   foreach($members as $member)
+   {
+      $db->members->update(array("_id" => $member["_id"]), array('$pull' => array("hours" => array("event_id" => $_GET["id"]))));
+   }
+
    $db->events->remove(array("_id" => $event_id));
 
    header("Location: http://" . $_SERVER["SERVER_NAME"] . "/events/admin/eventslist.php?message=deleteevent_success");
