bae3e3470eb76fcdfb23411b145a2280a627792a
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-12-21 00:27:14 -0800

    Added removal of hours if equal to 0
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/admin/editeventhours_process.php
+++ hours/admin/editeventhours_process.php
@@ -7,7 +7,7 @@
    $db->members->update(array(), array('$pull' => array("hours" => array("event_id" => $_POST["event-id"]))), array("multiple" => true));
    for ($i = 1; $i <= $_POST["attendee-count"]; $i++)
    {
-        if ($_POST["member-fname-" . $i] == "" || $_POST["member-hours-" . $i] == "" || $_POST["member-hours-" . $i] == "")
+        if ($_POST["member-fname-" . $i] == "" || $_POST["member-hours-" . $i] == "" || $_POST["member-hours-" . $i] == "" || $_POST["member-hours-" . $i] == "0")
 	   continue;
 	$db->members->update(array("fname" => $_POST["member-fname-" . $i], "lname" => $_POST["member-lname-" . $i]), array('$push' => array("hours" => array("event_id" => $_POST["event-id"], "hours" => $_POST["member-hours-" . $i]))));
    }
