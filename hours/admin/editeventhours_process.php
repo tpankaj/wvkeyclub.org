020d323287351e60c6553a7fb8b62f88ea0240ff
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2013-12-21 00:15:22 -0800

    Fixed bug preventing hours admin from editing
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/admin/editeventhours_process.php
+++ hours/admin/editeventhours_process.php
@@ -4,10 +4,11 @@
 
    $eventid = new MongoId($_POST["event-id"]);
 
-   $db->members->update(array(), array('$pull' => array("hours" => array("event_id" => $eventid))));
+   $db->members->update(array(), array('$pull' => array("hours" => array("event_id" => $_POST["event-id"]))));
    for ($i = 1; $i <= $_POST["attendee-count"]; $i++)
    {
         if ($_POST["member-fname-" . $i] == "" || $_POST["member-hours-" . $i] == "" || $_POST["member-hours-" . $i] == "")
+	   continue;
 	$db->members->update(array("fname" => $_POST["member-fname-" . $i], "lname" => $_POST["member-lname-" . $i]), array('$push' => array("hours" => array("event_id" => $_POST["event-id"], "hours" => $_POST["member-hours-" . $i]))));
    }
 
