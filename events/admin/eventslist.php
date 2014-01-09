eb16eab7c8f57a2c5602bd0593b0d72e7bafa24e
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-01-09 13:33:31 -0800

    Added delimiter to events list
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- events/admin/eventslist.php
+++ events/admin/eventslist.php
@@ -118,7 +118,7 @@
 		        echo "No";
 		     ?>
 		</td>
-		<td><a href="/events/admin/editevent.php?id=<?php echo $event["_id"]; ?>">Edit</a> <a href="#" data-reveal-id="delete-modal" data-reveal-ajax="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/events/admin/deleteevent_confirm.php?id=<?php echo $event["_id"]; ?>&name=<?php echo urlencode($event["name"]); ?>">Delete</a> <a href="/events/admin/signups.php?id=<?php echo $event["_id"]; ?>">View Signups</a></td>
+		<td><a href="/events/admin/editevent.php?id=<?php echo $event["_id"]; ?>">Edit</a> | <a href="#" data-reveal-id="delete-modal" data-reveal-ajax="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/events/admin/deleteevent_confirm.php?id=<?php echo $event["_id"]; ?>&name=<?php echo urlencode($event["name"]); ?>">Delete</a> | <a href="/events/admin/signups.php?id=<?php echo $event["_id"]; ?>">View Signups</a></td>
 	      </tr>
 	      <?php
 		 }
