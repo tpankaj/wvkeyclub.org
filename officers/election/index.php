8591a6f2741feb55598757723dcc39ccccb802a2
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-02-08 21:41:20 -0800

    Added 2014 club election page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ officers/election/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <title>Club Officers Election 2014 - Westview Key Club</title>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header.php"); ?>
+    <style>
+      .candidate
+      {
+         text-align:center;
+      }
+    </style>
+  </head>
+
+  <body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"); ?>
+
+    <header class="hide-for-small">
+      <div class="row">
+	<div class="twelve columns">
+	  <h1>Club Officers Election 2014</h1>
+	</div>
+      </div>
+    </header>
+
+    <div id="mainContent">
+
+      <div class="row show-for-small">
+	<div class="twelve columns">
+	  <h1>Club Officers Election 2014</h1>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <p>
+	    Here is a list of the candidates for the election taking place at lunch on Thursday, February 13, 2014 in L101.
+	  </p>
+	  <h3>President and Vice President</h3>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Amanda Du
+	    Class of 2016
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hey Key Club~ I'm Amanda and I'm running to be your President! When I first joined Key Club, I was one of those freshmen who thought "getting hours for college apps? Let's go!". Admit it, I wasn't the only one. But soon after, I realized that it isn't about getting hours, it's about spreading a smile, a positive word, and sharing a moment with others. Since that moment, I've been inspired to not only give back to Key Club, but extend this passion to other communities in which they're unable to experience it. When it comes to others, I am dedicated and motivated to do whatever is within my power to accomplish the best. I'm the kind of person who fits the “You'll hear her before you see her” saying. Most people would know me as the loud, smiling girl over there making a fool of herself. By becoming your president, I hope to spread and share the my love and devotion to Key Club to all of you. I've attended the Officer Training Conference, as well as the 2013 Fall Rally. Whether it's organizing events or shouting the Key Club chant, I've never lost the spirit of love and passion. To me, Key Club has been a place in which I can be and express myself. I want to ensure that Key Club continues to grow and extend their values outside our community. I believe that it can only be accomplished if we work together. Key Club is a family, we are one.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Shaoxiong Liu
+	    Class of 2015
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hello! My name is Shaoxiong Liu and I am currently a Junior running to be your next President. Throughout my years in Key Club, I have been very active and dedicated to the club by serving as Treasurer this year as well as attending many service events(Over 360 hours). I have organized fundraisers to raise money for charities such as PTP and Project Eliminate. With the help of YOU, members of the club, we have raised over $600 for PTP and are well on our way to our goal of raising $1000. I will use my knowledge and experiences gained from being the club Treasurer to help improve our club through my dedication and perseverance. I am also very responsible having turned in our club dues early this year and will be responsible for serving our club. Besides being active at the club level, I am also very involved in the divisional level of Key Club by attending Region Training Conference, Fall Rally, Divisional Council Meetings, as well as District Convention. The divisional level of Key Club has opened up a whole new world of Key Club to me and I hope to encourage you to become more active on the divisional level as well. We can all share our passion for service and spread it to others. If elected as President, it will be my goal to serve you, the members. Together, we can and will make a difference in our homes, schools, and communities. Thank You.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Lily Vo
+	    Class of 2015
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hello everyone! My name is Lily Vo and I'm running to be your next President for the 2014-2015 school year. You may know me as the crazy secretary trying to humor you behind the computer screen with varying, and questionable, success. Being a part for Key Club serving on both the appointed and executive board for three years, I have gathered and winnowed the tidbits and behind the scene tricks that has shaped me to be the officer I am today. Updating club hours, composing monthly report forms, and designing a new hours system has not only shown me the lengths my passion has driven me to do for the club, but has also taught me organization and time management skills that will be beneficial during my term as President and beneficial for the rest of my life. From electing the district board at DCON 2013 to stopping by McDonald's on the bus ride home from Fall Rally South, Key Club never fails to put a smile on my face and I'm sure it will never cease to do so. With great dedication and commitment to the club and division, I am ready to move one step further in the final year of Key Club career and fulfill my role as President and the responsibilities and duties that entails. Make the most out of your Key Club year and never stop stomping for service.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <h3>Secretary</h3>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Diandra Almasco
+	    Class of 2015
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hey guys! My name is Diandra Almasco and I'm running to be your next Key Club Secretary! I've been in Key Club since Freshmen Year when I first joined to do community service. As a sophomore I started to learn about the bigger parts of Key Club by attending Fall Rally. As a Junior, I am currently on Key Club's Appointed Board as your Interclub Commissioner! I have worked a lot with our Kiwanis branch while also working to increase the amount of community service with other clubs! I'm also an Events Officer for CSF, and I helped to connect events to Key Club, such as our Westview Creek Restoration Project. As a very organized and conscientious student, I will stay up to date with recording all of the hours for Key Clubbers while fulfilling my other duties to write reports on the progress and growth of our Club for the other branches of our division. Especially with the updated hours system, my ability to multitask will make keeping records, updating rosters, and writing reports feel like second nature to me! I hope to improve Key Club by making everything as straightforward as possible so that we can all continue our love for community service! I would love the opportunity to involve myself more in Key Club by not only being your Secretary, but also by being one of your leaders on the Executive Board. I believe that my experience on Appointed Board has allowed me to learn more about Westview Key Club, and will propel me forward to do even more for all of the members!
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Tushar Pankaj
+	    Class of 2015
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    I joined Key Club in my sophomore year and became involved seriously after going to DCON 2013. I currently hold the position of webmaster. I am running for secretary because I want to continue to serve the club after having the experience of serving as webmaster. Given the experience I received from writing a website to manage hours and events information, I feel that I'm most suited to hold the secretary position. If elected, I will work with the president and webmaster to handle the hours and member information as efficiently as possible. I will also keep members updated on what is going on in the club and upcoming events. In addition to performing the standard duties of the secretary, using my experience this year as webmaster, I want to improve the hours system to make viewing hours and requesting corrections as easy as possible. Another improvement I want to make is to keep all important information, like meeting minutes, organized digitally in order to save trees and make the information more easily accessible to everyone.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Audrey Trinh
+	    Class of 2017
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hi there! I am Audrey Trinh and currently a freshman. I'm running for secretary, because I want to be involved and show with my commitment to key club that I can try to do whatever in my power to make this club a fun and organized environment, for people who enjoy community service and making new friends. I plan to accomplish getting hours and event information updated or sent quickly and efficiently, as well as the other duties for this job. I would like to improve the process of club's emails that members have sent to the board. I would check and answer emails frequently everyday, because I know how it feels to wait for someone to respond or have no one respond at all. My goal is to have everyone to at least enjoy or get involved with key club to help out our community. I know I am pretty organize, excellent at keeping track on items, and turning work in without delay. I feel if I were chosen for secretary, I could make a difference in our club somehow using my skills, mind, and creativity.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Brandon Zhang
+	    Class of 2016
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hey everyone!
+	  </p>
+	  <p>
+	    My name is Brandon Zhang and I will be running for the secretary position to serve the upcoming year. I am currently a sophomore on the board as the general assistant and have been extremely devoted to the club, reaching over 75 hours of service in just this year! During this time, I've been actively involved in district level events such as Fall Rally and DCMs, learning what it is like to be a true Key Clubber. Attending these events has helped me develop and establish bonds between both members and officers from other clubs. As secretary I can utilize these connections to relay messages and ideas around our district. Also, I've attended both RTC and OTC secretary training conferences, learning and preparing myself to uphold the institutions of managing the club as a secretary. Through my experiences in these programs, I have gained appreciation for the secretary position and realized that was how I wanted to serve Key Club, you guys, and our community. Key Club means a lot to me because knowing that we are able to build our community, meet new people, and have fun is everything I could possibly wish for in an organization. As secretary, I want to express my dedication in helping to improve the club and to extend its limits. Thanks everyone and I'm looking forward to another amazing year!
+	  </p>
+	  <p>
+	    &#35;yolosweg&#35;ozairissohot
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <h3>Treasurer</h3>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Luqmaan Bokhary
+	    Class of 2017
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    I'm running for Treasurer because I would like to leave my mark at Westview Key Club. The things I will accomplish is having an organized way to handle revenue into our club. As Key Club International is trying to go green, as Treasurer I'll do my best to limit the use of paper in my job and file any reports electronically (unless otherwise necessary). I want to improve the relations between the Executive Board and our Key Club members so that they feel that they are in a safe, fun environment when they volunteer or simply come to one of our weekly meetings.
+	  </p>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="twelve columns">
+	  <h3>Bulletin Editor</h3>
+	</div>
+      </div>
+
+      <div class="row">
+	<div class="four columns candidate">
+	  <h4>
+	    Alec Chen
+	    Class of 2015
+	  </h4>
+	</div>
+	<div class="eight columns">
+	  <p>
+	    Hey guys! I'm running this year to be your Bulletin Editor for the school year of 2014-2015. As a Junior, I have been Westview Key Club's Bulletin Editor and am familiar with the requirements and duties for the job. Being in Key Club for all 3 years of high school, I've accumulated almost 200 hours of service and will strive to continue to serve the community better. I believe my reelection for this position will not only allow for me to serve my community in a more direct way, but also in a way that benefits the club at our school. As a previously elected Bulletin Editor, I am already accustomed to our Division's officers and members and hope to improve my relationship with them. I believe that my experience and interaction with other Key Clubbers over the past year has helped me become a better and more motivated person to help our community by becoming Westview Key Club's next Bulletin Editor. With my experience and involvment in the club, I hope to increase the amount of members in Key Club and motivate them to participate in more service events. I look forward to serving you as your next Bulletin Editor!
+	  </p>
+	</div>
+      </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
+  </body>
+
+</html>
