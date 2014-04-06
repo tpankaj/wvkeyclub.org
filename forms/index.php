c8ff6fba1df620b81f1e5b25f2ac880d34970c9e
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-04-05 22:26:25 -0700

    Updated forms page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- forms/index.php
+++ forms/index.php
@@ -31,19 +31,11 @@
       <div class="row">
 	<div class="small-12 columns">
 	  <p>
-	    To become a registered member, turn in all of these forms along with the $13 dues by November 21, 2013. After this date, the dues will go up to $15. Checks should be made payable to Westview Key Club.
+	    To become a registered member, turn in all of these forms along with the $15 dues. Checks should be made payable to Westview Key Club.
 	  </p>
 	  <p>
 	    <a href="/forms/Trip%20Permit.pdf">Trip Permit</a><br />
 	    <a href="/forms/Registration%20Form.pdf">Registration Form</a>
-	  </p>
-	  <h4>DCON 2014</h4>
-	  <p>
-	    For members that are attending DCON 2014, the following forms, along with the $350 payment, are due by Thursday, February 13, 2014.
-	  </p>
-	  <p>
-	    <a href="http://www.cnhkeyclub.org/downloads/District%20Convention/DCON%202012%20Registraton/Form%204%20Code%20of%20Conduct.pdf">Code of Conduct</a><br />
-	    <a href="http://www.cnhkeyclub.org/downloads/Resources/Policy%20and%20Forms/Medical%20Form.pdf">Medical Release</a>
 	  </p>
 	</div>
       </div>
