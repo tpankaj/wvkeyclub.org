ad4105d91c8b44b3e485aa1b3f6a8776c78b164c
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-08-16 18:04:05 -0700

    New about page
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- about-us/index.php
+++ about-us/index.php
@@ -31,8 +31,29 @@
       <div class="row">
 	<div class="small-9 columns">
 	  <h3>What Is Key Club?</h3>
+	  <h4>Westview Key Club</h4>
 	  <p>
-	    Key Club International is the largest high school organization devoted to providing service to communities. Westview Key Club is the largest community service club on Westview's campus. We meet every Thursday at lunch in L101 (Oydna). Worldwide, there are 250,000 members, 5,000 clubs, and participation in 31 countries. The first Key Club originated in May 1925 at Sacramento High School in California. This club was sponsored by the Kiwanis Club of Sacramento and this first club's original idea was to create a vocational guidance program to stop the problems of fraternities. From this beginning group came the idea of a service organization aimed at bettering the home.
+	    Westview Key Club is the largest community service club on campus. We meet every Thursday at lunch in room L101 (Oydna).
+	  </p>
+	  <h4>Key Club International</h4>
+	  <p>
+	    With over a quarter million members and 5,000 clubs worldwide, Key Club International is a student-led organization that provides its members with opportunities to provide service, build character, and develop leadership. Sponsored by Kiwanis International, high school student members of Key Club perform acts of service in their communities. Key Club International also fosters great relations with March of Dimes, Children’s Miracle Network, and UNICEF, targeting their fundraisers to benefit these three major organizations and serve children of the world.
+	  </p>
+	  <h3>How to Get Involved</h3>
+	  <p>
+	    Getting involved in Westview Key Club is one of the most rewarding and worthwhile experiences in your high school career. You will lend a helping hand to the community while meeting other peers who are all dedicated to giving back to their home, school, and community. Getting involved in Westview Key Club is very easy and only requires two things: your name and your email address. 
+	  </p>
+	  <p>
+	    Many people falsely believe that you must be a registered member to attend our events. This is untrue! If you are unsure if you want to join Key Club, you may stop by and come to our meetings or attend an event or two before you complete your registration forms. However, registering for Key Club will give you many benefits such as attending Region Training Conference, District Convention, Fall Rally at Six Flags and from receiving end of the year plaques and certificates.
+	  </p>
+	  <p>
+	    To sign up for an event, click on Events on the tabs bar. Proceed by choosing Events Sign-Up and enter your email address and choose your desired event from the list provided. You may also sign up for an event and hear our latest updates by attending our meetings.
+	  </p>
+	  <p>
+	    The registration process takes place in the beginning of the year (dates will be announced shortly). You must complete a Trip Permit, WVKC Registration Form, and pay $13 to join Key Club. Once you have registered under the California-Nevada-Hawaii District you will receive a membership card and a Key Club pin. You will then be added to our mailing list and receive updates via <a href="mailto:wvkeyclub@gmail.com">wvkeyclub@gmail.com</a>. If you join Key Club later in the year, a late registration fee of $15 is enforced.
+	  </p>
+	  <p>
+	    Tell your friends to join Key Club and join us on our adventures of the 2014-2015 year.
 	  </p>
 	  <h3> Key Club Pledge</h3>
 	  <p>
