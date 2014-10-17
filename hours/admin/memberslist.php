c69c93f117141a9bffc99c1bccb6aff48490399a
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-10-16 17:51:29 -0700

    Still trying to fix members list delete modal
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- hours/admin/memberslist.php
+++ hours/admin/memberslist.php
@@ -91,7 +91,7 @@
 		        echo "No";
 		     ?>
 		</td>
-		<td><a href="/hours/admin/editmember.php?id=<?php echo $member["_id"]; ?>">Edit</a> <a href="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/hours/admin/deletemember_confirm.php?id=<?php echo $member["_id"]; ?>&fname=<?php echo urlencode($member["fname"]); ?>&lname=<?php echo urlencode($member["lname"]); ?>" data-reveal-id="delete-modal" data-reveal-ajax="true">Delete</a></td>
+		<td><a href="/hours/admin/editmember.php?id=<?php echo $member["_id"]; ?>">Edit</a> <a href="#" data-reveal-id="delete-modal" data-reveal-ajax="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/hours/admin/deletemember_confirm.php?id=<?php echo $member["_id"]; ?>&fname=<?php echo urlencode($member["fname"]); ?>&lname=<?php echo urlencode($member["lname"]); ?>">Delete</a></td>
 	      </tr>
 	      <?php
 		 }
